cf0a039e7e545b819b7093ab41935b7fceb3e0fb
Bruno Trindade <brunotrndd@gmail.com>
2021-11-27 16:39:45 -0300

    Faz requisição e toggle button progress

--- src/context/InitialPageProvider.js
+++ src/context/InitialPageProvider.js
@@ -6,12 +6,15 @@ import InitialPageContext from './InitialPageContext';
 function InitialPageProvider({ children }) {
   const [next, setNext] = useState(false);
   const [nQuestions, setNQuestions] = useState(NaN);
+  const [questions, setQuestions] = useState([]);
 
   const contextValue = {
     next,
     setNext,
     nQuestions,
     setNQuestions,
+    questions,
+    setQuestions,
   };
 
   return (
--- src/pages/Start.js
+++ src/pages/Start.js
@@ -1,32 +1,65 @@
-import React, { useContext } from 'react';
-import { Button, Card, CardContent, Typography } from '@mui/material';
+import React, { useContext, useState } from 'react';
+import { useHistory } from 'react-router-dom';
+import { Button, Card, CardContent, CircularProgress, Typography } from '@mui/material';
 import InitialPageContext from '../context/InitialPageContext';
+import axios from 'axios';
 
 function Start() {
-  const { nQuestions, setNext } = useContext(InitialPageContext);
+  const { nQuestions, setNext, setQuestions } = useContext(InitialPageContext);
+  const [isFetch, setIsFetch] = useState(false);
+  const history = useHistory();
+
+  const options = {
+    url: `https://opentdb.com/api.php?amount=${nQuestions}`,
+  }
+
+  async function handleStartBtn() {
+    setIsFetch(true);  
+    const request = await axios(options);
+    console.log(request);
+    setQuestions(request.data.results);
+    history.push('/questions');
+  }
+
   return(
     <Card sx={{ mx: '20px', mt: '100px', maxWidth: 'sm', minWidth: 275 }} fixed>
       <CardContent>
         <Typography variant='h7'>You selected { nQuestions } questions.</Typography>
-        <Button
-            sx={{ mt: '10px', maxWidth: '300px' }}
-            color="primary"
-            variant="contained"
-            fullWidth
-            type="submit"
-          >
-            Start
-          </Button>
-          <Button
-            sx={{ mt: '10px', maxWidth: '300px' }}
-            color="error"
-            variant="outlined"
-            fullWidth
-            type="submit"
-            onClick={ () => setNext(false) }
-          >
-            Cancel
-          </Button>
+        <div>
+          { isFetch ? (  
+            <Button
+                sx={{ mt: '10px', maxWidth: '300px' }}
+                color="primary"
+                variant="contained"
+                fullWidth
+                type="submit"
+                onClick={ handleStartBtn }
+              >
+                <CircularProgress color="inherit" />
+              </Button>
+            ) : (
+              <Button
+                sx={{ mt: '10px', maxWidth: '300px' }}
+                color="primary"
+                variant="contained"
+                fullWidth
+                type="submit"
+                onClick={ handleStartBtn }
+              >
+                Start
+              </Button>
+            ) }
+            <Button
+              sx={{ mt: '10px', maxWidth: '300px' }}
+              color="error"
+              variant="outlined"
+              fullWidth
+              type="submit"
+              onClick={ () => setNext(false) }
+            >
+              Cancel
+            </Button>
+        </div>
       </CardContent>
     </Card>
   )
